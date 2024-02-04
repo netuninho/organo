04e805947e69ceb0560b30dadf445a8ad94562d8
Manu <manuelass.dev@gmail.com>
2024-02-04 19:44:35 -0300

    Criando componente Rodape

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
+import Rodape from './componentes/Rodape';
 
 function App() {
 
@@ -60,6 +61,7 @@ function App() {
         corSecundaria={time.corSecundaria}
         cadastros={cadastros.filter(cadastro => cadastro.time === time.nome)}
       />)}
+      <Rodape/>
     </div>
   );
 }
---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,37 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (
+        <footer className='rodape'>
+            <section>
+                <ul>
+                    <li>
+                        <a href="facebook.com" target="_blank">
+                            <img src="/imagens/fb.png" alt="Logo Facebook" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="twitter.com" target="_blank">
+                            <img src="/imagens/tw.png" alt="Logo Twitter" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="instagram.com" target="_blank">
+                            <img src="/imagens/ig.png" alt="Logo Instagram" />
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src="/imagens/logo.png" alt="Logo Organo" />
+            </section>
+            <section>
+                <p>
+                    Desenvolvido por Alura.
+                </p>
+            </section>
+        </footer>
+    )
+}
+
+export default Rodape
